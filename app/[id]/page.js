aef406d70d7991083b706578cf05d559fe2de733
Mateo Algañarás <tutealga@gmail.com>
2023-11-03 00:34:58 -0300

    Change cards background

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -11,12 +11,12 @@ import { getUser, getRepos, getStars, getCommits, getIssues, getPr } from '@/app
 
 const getRank = async ({level}) => {
   const ranks = {
-    "Diamante": {border:'border-[#70d1f4] shadow-[inset_0_0_70px_0px_rgba(112,209,244,0.5)]', text:'text-[#70d1f4]', borderb: 'border-b-[#70d1f4]'}, 
-    "Oro": {border:'border-[#ffd700] shadow-[inset_0_0_70px_0px_rgba(255,215,0,0.5)]', text:'text-[#ffd700]', borderb: 'border-b-[#ffd700]'}, 
-    "Bronze": {border:'border-[#cd7f32] shadow-[inset_0_0_70px_0px_rgba(205,127,50,0.5)]', text:'text-[#cd7f32]', borderb: 'border-b-[#cd7f32]'},
-    "Plata": {border:'border-[#bec2cb] shadow-[inset_0_0_70px_0px_rgba(190,194,203,0.5)]', text:'text-[#bec2cb]', borderb: 'border-b-[#bec2cb]'},
-    "Platino": {border:'border-[#046307] shadow-[inset_0_0_70px_0px_rgba(4,99,7,0.5)]', text:'text-[#046307]', borderb: 'border-b-[#046307]'},
-    "Maestro": {border:'border-[#884dA7] shadow-[inset_0_0_70px_0px_rgba(136,77,167,0.5)]', text:'text-[#884dA7]', borderb: 'border-b-[#884dA7]'}
+    "Diamante": {border:'border-[#70d1f4] bg-[linear-gradient(120deg,rgb(0,61,83,0.1)0%,rgb(112,209,244,0.1)100%)]', text:'text-[#70d1f4]', borderb: 'border-b-[#70d1f4]'}, 
+    "Oro": {border:'border-[#ffd700] bg-[linear-gradient(120deg, rgba(179,156,33,0.1)0%,rgb(255,215,0,0.1)100%)]', text:'text-[#ffd700]', borderb: 'border-b-[#ffd700]'}, 
+    "Bronze": {border:'border-[#cd7f32] bg-[linear-gradient(120deg,rgb(205,127,50,0.1)0%,rgb(255,127,0,0.1)100%)]', text:'text-[#cd7f32]', borderb: 'border-b-[#cd7f32]'},
+    "Plata": {border:'border-[#bec2cb] bg-[linear-gradient(120deg,rgb(190,194,203,0.1)0%,rgb(255,255,255,0.1)100%)]', text:'text-[#bec2cb]', borderb: 'border-b-[#bec2cb]'},
+    "Platino": {border:'border-[#046307] bg-[linear-gradient(120deg,rgb(4,99,7,0.1)0%,rgb(0,255,8,0.1)100%)]', text:'text-[#046307]', borderb: 'border-b-[#046307]'},
+    "Maestro": {border:'border-[#884dA7] bg-[linear-gradient(120deg,rgb(136,77,167,0.1)0%,rgb(167,0,255,0.1)100%)]', text:'text-[#884dA7]', borderb: 'border-b-[#884dA7]'}
   }
 
   const ranking = Object.keys(ranks).find((res) => res === level)
@@ -46,7 +46,7 @@ const hexRank = await getRank({level})
       <Image src={user.avatar_url} width={50} height={50} className="rounded-full w-14 h-14"/>
      <p className="text-white flex font-bold items-center">{user.name}</p>
         </div>
-     <FontAwesomeIcon title={`Top: ${percentile.toFixed(1)}%`} className={`${hexRank.text} w-10 h-10`} icon={faAward} flip />
+     <FontAwesomeIcon title={`Top: ${percentile.toFixed(1)}%`} className={`${hexRank.text} w-10 h-10`} icon={faAward} />
       </div>
      <div className="grid grid-cols-2 gap-4">
      <div className="flex flex-col text-sm"><div className="flex gap-1 items-center"><FontAwesomeIcon className="w-4 h-4" icon={faStar} /><span>Stars:</span></div><p className="font-bold">{stars > 1000 ? `${(stars / 1000).toFixed(1)}k`: stars}</p></div>
