1e9bb3363e6cce0c34190308bf1368cb671ff131
Mateo Algañarás <tutealga@gmail.com>
2023-11-07 19:48:54 -0300

    Add hover footer links

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,14 +5,14 @@ import Link from 'next/link'
 const Footer = () => {
     
     return (
-    <footer className="flex gap-2 opacity-20 text-sm">
-        © 2023 ·
-        <Link className="flex gap-1 items-center justify-center" href="https://www.linkedin.com/in/mateoalganaras/">
+    <footer className="flex gap-2 text-sm">
+        <p className="opacity-20">© 2023 ·</p>
+        <Link className="hover:opacity-100 opacity-20 flex gap-1 items-center justify-center" href="https://www.linkedin.com/in/mateoalganaras/">
             <FontAwesomeIcon className="w-4 h-4" icon={faLinkedin} />
             <span>/mateoalganaras</span>
         </Link>
-        <p>-</p>
-        <Link className="flex gap-1 items-center justify-center" href="https://github.com/Tutealga">
+        <p className="opacity-20">-</p>
+        <Link className="hover:opacity-100 opacity-20 flex gap-1 items-center justify-center" href="https://github.com/Tutealga">
         <FontAwesomeIcon className="w-4 h-4" icon={faGithub} />
             <span>Tutealga</span>
         </Link>
