bb574f98733925a05a7c32923db1fff5fd5aafef
Mateo Algañarás <tutealga@gmail.com>
2023-11-11 19:04:04 -0300

    Add stroke width on component

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -15,11 +15,11 @@ import Rank from "../components/Rank";
 
 const getRank = async ({level}) => {
   const ranks = {
-    "Diamante": {border:'border-[#70d1f4] bg-[#70d1f4]', borderb: 'border-b-[#70d1f4]', shadow: 'shadow-[0_51px_2px_-30px_#8cebff_inset,54px_52px_2px_-30px_#7edeff_inset,_0px_-64px_2px_-30px_#62c4e7_inset]', stroke: 'stroke-[#44abcd] stroke-[1.5rem]', fill:'diamond'}, 
-    "Oro": {border:'border-[#ffd700] bg-[#ffd700]', borderb: 'border-b-[#ffd700]', shadow: 'shadow-[0_51px_2px_-30px_#fff438_inset,54px_52px_2px_-30px_#ffe524_inset,_0px_-64px_2px_-30px_#efc900_inset]', stroke: 'stroke-[#d0ad00] stroke-[1.5rem]', fill:'gold'}, 
-    "Bronze": {border:'border-[#a97142] bg-[#a97142]', borderb: 'border-b-[#a97142]', shadow: 'shadow-[0_51px_2px_-30px_#bb8151_inset,54px_52px_2px_-30px_#b27949_inset,_0px_-64px_2px_-30px_#a0693b_inset]', stroke: 'stroke-[#8f5a2d] stroke-[1.5rem]', fill:'bronze'},
-    "Plata": {border:'border-[#bec2cb] bg-[#bec2cb]', borderb: 'border-b-[#bec2cb]', shadow: 'shadow-[0_51px_2px_-30px_#d7dbe5_inset,54px_52px_2px_-30px_#cbcfd8_inset,_0px_-64px_2px_-30px_#b2b5be_inset]', stroke: 'stroke-[#999da6] stroke-[1.5rem]', fill:'silver'},
-    "Platino": {border:'border-[#50c878] bg-[#50c878]', borderb: 'border-b-[#50c878]', shadow: 'shadow-[0_51px_2px_-30px_#69e08e_inset,54px_52px_2px_-30px_#5dd483_inset,_0px_-64px_2px_-30px_#43bc6d_inset]', stroke: 'stroke-[#24a558] stroke-[1.5rem]', fill:'emerald'}
+    "Diamante": {border:'border-[#70d1f4] bg-[#70d1f4]', borderb: 'border-b-[#70d1f4]', shadow: 'shadow-[0_51px_2px_-30px_#8cebff_inset,54px_52px_2px_-30px_#7edeff_inset,_0px_-64px_2px_-30px_#62c4e7_inset]', stroke: 'stroke-[#44abcd]', fill:'diamond'}, 
+    "Oro": {border:'border-[#ffd700] bg-[#ffd700]', borderb: 'border-b-[#ffd700]', shadow: 'shadow-[0_51px_2px_-30px_#fff438_inset,54px_52px_2px_-30px_#ffe524_inset,_0px_-64px_2px_-30px_#efc900_inset]', stroke: 'stroke-[#d0ad00]', fill:'gold'}, 
+    "Bronze": {border:'border-[#a97142] bg-[#a97142]', borderb: 'border-b-[#a97142]', shadow: 'shadow-[0_51px_2px_-30px_#bb8151_inset,54px_52px_2px_-30px_#b27949_inset,_0px_-64px_2px_-30px_#a0693b_inset]', stroke: 'stroke-[#8f5a2d]', fill:'bronze'},
+    "Plata": {border:'border-[#bec2cb] bg-[#bec2cb]', borderb: 'border-b-[#bec2cb]', shadow: 'shadow-[0_51px_2px_-30px_#d7dbe5_inset,54px_52px_2px_-30px_#cbcfd8_inset,_0px_-64px_2px_-30px_#b2b5be_inset]', stroke: 'stroke-[#999da6]', fill:'silver'},
+    "Platino": {border:'border-[#50c878] bg-[#50c878]', borderb: 'border-b-[#50c878]', shadow: 'shadow-[0_51px_2px_-30px_#69e08e_inset,54px_52px_2px_-30px_#5dd483_inset,_0px_-64px_2px_-30px_#43bc6d_inset]', stroke: 'stroke-[#24a558]', fill:'emerald'}
   }
 
   const ranking = Object.keys(ranks).find((res) => res === level)
--- app/components/Rank.js
+++ app/components/Rank.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Rank = ({shadow, stroke, star}) => {
 
   return (
-        <div className={`pentagono w-12 h-12 flex items-center justify-center ${shadow}`}><FontAwesomeIcon className={`w-8 h-8 ${star} ${stroke}`} icon={faStar} /></div>
+        <div className={`pentagono w-12 h-12 flex items-center justify-center ${shadow}`}><FontAwesomeIcon className={`w-8 h-8 ${star} stroke-[1.5rem] ${stroke}`} icon={faStar} /></div>
           )
 }
 
