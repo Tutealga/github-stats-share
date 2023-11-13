76e636e8326b5f295ce51a08634a83e0d871c481
Mateo Algañarás <tutealga@gmail.com>
2023-11-13 00:50:17 -0300

    Title gramatical correction

--- app/layout.js
+++ app/layout.js
@@ -5,11 +5,11 @@ import RankLayout from './components/RankLayout'
 import { ranks } from './const/ranks'
 
 export const metadata = {
-  title: 'GitHub Stats for share',
+  title: 'GitHub stats to share',
   description: 'Search your GitHub stats and share on social networks',
   metadataBase: new URL('https://github-stats-linkedin.vercel.app'),
   openGraph: {
-    title: 'GitHub stats for share on social networks',
+    title: 'GitHub stats to share on social networks',
     description: 'Search your GitHub stats and share on social networks',
     images: '/opengraph-image.jpg',
     url: 'https://github-stats-linkedin.vercel.app',
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
     <html>
       <body className="relative bg-[#101a23] text-white max-h-screen p-4">
           <section className="flex flex-col items-center justify-center p-5 w-[400px] m-auto gap-4">
-          <h1 className="text-2xl font-bold">GitHub stats for share</h1>
+          <h1 className="text-2xl font-bold">GitHub stats to share</h1>
           <section className="grid grid-cols-3 items-center sm:flex justify-center gap-4">
             {ranks.map(rank => <article key={rank.name} className="flex items-center justify-center gap-1"><RankLayout star={rank.fill} shadow={rank.shadow} stroke={rank.stroke}/>{rank.name}</article>)}
           </section>
