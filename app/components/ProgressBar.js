b267d03c6456f56064eff3f9a075a9b79c0ef11a
Mateo Algañarás <tutealga@gmail.com>
2023-11-16 00:33:38 -0300

    Add next rank progress bar

--- app/components/ProgressBar.js
+++ app/components/ProgressBar.js
@@ -1,9 +1,11 @@
 import RankLayout from './RankLayout'
 
 const ProgressBar = ({ nextLevelPercentage, nextLevel, bg }) => {
+  const levelPercentage = nextLevelPercentage > 100 ? 100 : nextLevelPercentage.toFixed(1)
+
   return (
-    <div title={`${nextLevelPercentage > 100 ? 100 : (nextLevelPercentage).toFixed(1)}%`} className={`h-[4px] w-full rounded-md ${bg} bg-opacity-20 flex items-center justify-between`}>
-      <div className={`h-full rounded-md ${nextLevel.background}`} style={{ width: `${nextLevelPercentage > 100 ? 100 : nextLevelPercentage}%` }} />
+    <div title={`${levelPercentage}%`} className={`h-[4px] w-full rounded-md ${bg} bg-opacity-20 flex items-center justify-between`}>
+      <div className={`h-full rounded-md ${nextLevel.background}`} style={{ width: `${levelPercentage}%` }} />
       <RankLayout star={nextLevel.fill} shadow={nextLevel.shadow} stroke={nextLevel.stroke} />
     </div>
   )
