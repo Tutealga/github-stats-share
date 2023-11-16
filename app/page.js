afac03cf28004a3f2151858a458b7993ef393eb1
Mateo Algañarás <tutealga@gmail.com>
2023-11-16 01:06:36 -0300

    Add next rank progress bar

--- app/page.js
+++ app/page.js
@@ -7,13 +7,18 @@ import { homeUser } from './const/homeUser'
 
 export default function Home () {
   const [rank, setRank] = useState(ranks[0])
+  const [nextRank, setNextRank] = useState(ranks[0])
 
   useEffect(() => {
     const length = ranks.length
 
     const intervalId = setInterval(() => {
-      const randomKey = ranks[Math.floor(Math.random() * length)]
+      const index = Math.floor(Math.random() * length)
+      const randomKey = ranks[index]
+      const nextRandomKey = ranks[index !== 0 ? index - 1 : index]
+      console.log(randomKey, nextRandomKey)
       setRank(randomKey)
+      setNextRank(nextRandomKey)
     }, 2500)
 
     return () => {
@@ -21,5 +26,5 @@ export default function Home () {
     }
   }, [])
 
-  return <ProfileCard hexRank={rank} user={homeUser} stars={24000} pr={729} commits={32206} issues={133} />
+  return <ProfileCard hexRank={rank} user={homeUser} stars={24000} pr={729} commits={32206} issues={133} nextLevelPercentage={rank.name === 'Diamond' ? 100 : Math.floor(Math.random() * 100)} nextLevel={nextRank} />
 }
