eee00946720a02b797cf75da5993a6549857c317
Mateo Algañarás <tutealga@gmail.com>
2023-11-28 18:10:15 -0300

    New ui

--- app/components/Search.js
+++ app/components/Search.js
@@ -17,7 +17,7 @@ const Search = () => {
     <div className='pl-2 border border-white border-opacity-40 space-x-1 flex w-full justify-between flex-row rounded-lg'>
       <form className='w-full item-center justify-between flex' onSubmit={(e) => handleSubmit(e)}>
         <input className='w-full bg-transparent text-sm font-bold font-mono focus:outline-none p-2' placeholder='GitHub username...' value={user} onChange={(e) => setUser(e.target.value)} />
-        <button className={`bg-[rgb(33,38,45,1)] py-1 px-2 rounded-r-lg p-2 ${user && 'bg-[#E0004D] cursor-pointer'} cursor-not-allowed`} type='submit'>
+        <button className={`py-1 px-2 rounded-r-lg p-2 ${user ? 'bg-[#E0004D] cursor-pointer' : 'bg-[rgb(33,38,45,1)] cursor-not-allowed'}`} type='submit'>
           <p>Search</p>
         </button>
       </form>
