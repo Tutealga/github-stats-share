5d87d42cd75b6f310cf5ca248cade2ccccc5ef27
Mateo Algañarás <tutealga@gmail.com>
2023-11-28 19:19:44 -0300

    New README

--- app/const/ranks.js
+++ app/const/ranks.js
@@ -1,7 +1,42 @@
 export const ranks = [
-  { name: 'Diamond', border: 'border-[#70d1f4]', background: 'bg-[#70d1f4]', shadow: 'shadow-[0_51px_2px_-30px_#8cebff_inset,54px_52px_2px_-30px_#7edeff_inset,_0px_-64px_2px_-30px_#62c4e7_inset]', stroke: 'stroke-[#44abcd]', fill: 'diamond' },
-  { name: 'Emerald', border: 'border-[#50c878]', background: 'bg-[#50c878]', shadow: 'shadow-[0_51px_2px_-30px_#69e08e_inset,54px_52px_2px_-30px_#5dd483_inset,_0px_-64px_2px_-30px_#43bc6d_inset]', stroke: 'stroke-[#24a558]', fill: 'emerald' },
-  { name: 'Gold', border: 'border-[#ffd700]', background: 'bg-[#ffd700]', shadow: 'shadow-[0_51px_2px_-30px_#fff438_inset,54px_52px_2px_-30px_#ffe524_inset,_0px_-64px_2px_-30px_#efc900_inset]', stroke: 'stroke-[#d0ad00]', fill: 'gold' },
-  { name: 'Silver', border: 'border-[#bec2cb]', background: 'bg-[#bec2cb]', shadow: 'shadow-[0_51px_2px_-30px_#d7dbe5_inset,54px_52px_2px_-30px_#cbcfd8_inset,_0px_-64px_2px_-30px_#b2b5be_inset]', stroke: 'stroke-[#999da6]', fill: 'silver' },
-  { name: 'Bronze', border: 'border-[#a97142]', background: 'bg-[#a97142]', shadow: 'shadow-[0_51px_2px_-30px_#bb8151_inset,54px_52px_2px_-30px_#b27949_inset,_0px_-64px_2px_-30px_#a0693b_inset]', stroke: 'stroke-[#8f5a2d]', fill: 'bronze' }
+  {
+    name: 'Diamond',
+    border: 'border-[#70d1f4]',
+    background: 'bg-[#70d1f4]',
+    shadow: 'shadow-[0_51px_2px_-30px_#8cebff_inset,54px_52px_2px_-30px_#7edeff_inset,_0px_-64px_2px_-30px_#62c4e7_inset]',
+    stroke: 'stroke-[#44abcd]',
+    fill: 'diamond'
+  },
+  {
+    name: 'Emerald',
+    border: 'border-[#50c878]',
+    background: 'bg-[#50c878]',
+    shadow: 'shadow-[0_51px_2px_-30px_#69e08e_inset,54px_52px_2px_-30px_#5dd483_inset,_0px_-64px_2px_-30px_#43bc6d_inset]',
+    stroke: 'stroke-[#24a558]',
+    fill: 'emerald'
+  },
+  {
+    name: 'Gold',
+    border: 'border-[#ffd700]',
+    background: 'bg-[#ffd700]',
+    shadow: 'shadow-[0_51px_2px_-30px_#fff438_inset,54px_52px_2px_-30px_#ffe524_inset,_0px_-64px_2px_-30px_#efc900_inset]',
+    stroke: 'stroke-[#d0ad00]',
+    fill: 'gold'
+  },
+  {
+    name: 'Silver',
+    border: 'border-[#bec2cb]',
+    background: 'bg-[#bec2cb]',
+    shadow: 'shadow-[0_51px_2px_-30px_#d7dbe5_inset,54px_52px_2px_-30px_#cbcfd8_inset,_0px_-64px_2px_-30px_#b2b5be_inset]',
+    stroke: 'stroke-[#999da6]',
+    fill: 'silver'
+  },
+  {
+    name: 'Bronze',
+    border: 'border-[#a97142]',
+    background: 'bg-[#a97142]',
+    shadow: 'shadow-[0_51px_2px_-30px_#bb8151_inset,54px_52px_2px_-30px_#b27949_inset,_0px_-64px_2px_-30px_#a0693b_inset]',
+    stroke: 'stroke-[#8f5a2d]',
+    fill: 'bronze'
+  }
 ]
