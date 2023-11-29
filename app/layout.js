0693690cc9e39139ab086ce3a6b523de1d96635b
Mateo Algañarás <tutealga@gmail.com>
2023-11-29 00:43:42 -0300

    Change title

--- app/layout.js
+++ app/layout.js
@@ -22,10 +22,10 @@ export default function RootLayout ({ children }) {
       <body className='relative bg-[#05060f] text-white max-h-screen'>
         <section className='flex flex-col items-center justify-center p-4 w-[400px] m-auto gap-5'>
           <h1 className='text-4xl font-bold opacity-90'>GitHub Stats to Share</h1>
+          <Search />
           <section className='grid grid-cols-3 items-center sm:flex justify-center gap-4'>
             {ranks.map(rank => <article key={rank.name} className='flex items-center justify-center gap-1'><RankLayout star={rank.fill} shadow={rank.shadow} stroke={rank.stroke} /><span className='opacity-80'>{rank.name}</span></article>)}
           </section>
-          <Search />
           {children}
           <Footer />
         </section>
